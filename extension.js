2a32d90d91ba1e29f81e56a85b3351371931b9e5
RahulThummar <rthummar788@rku.ac.in>
2025-07-13 00:07:43 +0530

    Added code into the extension.js

--- extension.js
+++ extension.js
@@ -1,42 +1,105 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "conditionallogger" is now active!'
-  );
+  console.log('ConditionalLogger is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     "conditionallogger.ConditionalLogger",
-    function () {
-      // The code you place here will be executed every time your command is executed
+    async function () {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No active editor found.");
+        return;
+      }
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from ConditionalLogger!"
-      );
+      const document = editor.document;
+      const selection = editor.selection;
+
+    //   if (selection.isEmpty) {
+    //     vscode.window.showWarningMessage("Please select code with conditionals (if, else, switch).");
+    //     return;
+    //   }
+
+      const selectedText = document.getText(selection);
+      const startLine = selection.start.line;
+
+      let ast;
+      try {
+        ast = parser.parse(selectedText, {
+          sourceType: "module",
+          plugins: ["jsx"]
+        });
+      } catch (err) {
+        vscode.window.showErrorMessage("Failed to parse selected code.");
+        console.error(err);
+        return;
+      }
+
+      const edits = [];
+
+      traverse(ast, {
+        IfStatement(path) {
+          const node = path.node;
+
+          // Insert log for `if` block
+          if (node.consequent && node.consequent.loc) {
+            const insertLine = startLine + node.consequent.loc.start.line + 1;
+            const logText = `console.log("Entered if block at line ${insertLine}");\n`;
+            edits.push({ line: insertLine, text: logText });
+          }
+
+          // Insert log for `else` block (if it exists)
+          if (node.alternate && node.alternate.loc) {
+            const insertLine = startLine + node.alternate.loc.start.line + 1;
+            const logText = `console.log("Entered else block at line ${insertLine}");\n`;
+            edits.push({ line: insertLine, text: logText });
+          }
+        },
+
+        SwitchStatement(path) {
+          const node = path.node;
+          const insertLine = startLine + node.loc.start.line + 1;
+          const logText = `console.log("Entered switch block at line ${insertLine}");\n`;
+          edits.push({ line: insertLine, text: logText });
+        },
+
+        SwitchCase(path) {
+          const node = path.node;
+          const insertLine = startLine + node.loc.start.line + 1;
+          const caseLabel = node.test ? `case ${node.test.value}` : "default";
+          const logText = `console.log("Entered ${caseLabel} block at line ${insertLine}");\n`;
+          edits.push({ line: insertLine, text: logText });
+        }
+      });
+
+      if (edits.length === 0) {
+        vscode.window.showInformationMessage("No conditional blocks found in selected code.");
+        return;
+      }
+
+      await editor.edit((editBuilder) => {
+        for (let i = edits.length - 1; i >= 0; i--) {
+          const { line, text } = edits[i];
+          const position = new vscode.Position(line, 0);
+          editBuilder.insert(position, text);
+        }
+      });
+
+    //   vscode.window.showInformationMessage("Logs inserted into conditional blocks!");
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate,
+  deactivate
 };
